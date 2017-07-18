f3c1abbe0409980096228b922e2af341985e2be4
Sujit Roy <kumarsujitroy@gmail.com>
2017-07-18 07:01:08 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,7 +130,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_NO_SECURE_DISCARD := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/Android.mk
 
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
